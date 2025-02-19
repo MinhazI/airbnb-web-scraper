3a28e5eb47939846dab2a113f734ddd7f430193b
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2025-02-19 11:47:22 +0530

    Fixed the loader's look and feel

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -4,12 +4,12 @@ export const Loader = () => {
       {[...Array(8)].map((_: undefined, index: number) => (
         <div
           role="status"
-          className="max-w-sm p-4 border border-gray-200 rounded-sm shadow-sm animate-pulse md:p-6 dark:border-gray-700"
+          className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 animate-pulse"
           key={index}
         >
-          <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded-sm dark:bg-gray-700">
+          <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded-sm dark:bg-gray-700 rounded-t-lg">
             <svg
-              className="w-10 h-10 text-gray-200 dark:text-gray-600"
+              className="w-[500px] h-10 text-gray-200 dark:text-gray-600"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -19,14 +19,16 @@ export const Loader = () => {
               <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
             </svg>
           </div>
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-          <div className="flex items-center mt-4">
-            <div>
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
-              <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          <div className=" p-5">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            <div className="flex items-center mt-4">
+              <div>
+                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+              </div>
             </div>
           </div>
           <span className="sr-only">Loading...</span>
