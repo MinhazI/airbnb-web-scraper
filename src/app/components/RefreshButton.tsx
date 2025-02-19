4ec554a11392d9558563a0e89bc8ca29e6333375
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2025-02-19 13:07:46 +0530

    Wrote the logic to allow users to refresh every 30 mins to prevent abuse of the scraper

--- src/app/components/RefreshButton.tsx
+++ src/app/components/RefreshButton.tsx
@@ -1,18 +1,89 @@
+import { useEffect, useState } from "react";
+
 interface props {
   callback: () => Promise<void>;
   loading: boolean;
 }
 
 const RefreshButton = ({ callback, loading }: props) => {
+  const [buttonDisable, setButtonDisable] = useState(loading);
+  const [timeSessionStorage, setTimeSessionStorage] = useState<string | null>(
+    null
+  );
+  const compare = 30 * 60 * 1000;
+  const [countdown, setCountdown] = useState<number>(30 * 60 * 1000);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  useEffect(() => {
+    const storedTime = window.sessionStorage.getItem("refresh");
+
+    if (storedTime) {
+      setTimeSessionStorage(storedTime);
+      const dif = new Date().getTime() - Number(storedTime);
+      const remainingTime = compare - dif;
+
+      if (remainingTime > 0) {
+        setCountdown(remainingTime);
+        setButtonDisable(true);
+      } else {
+        setButtonDisable(false);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (countdown > 0) {
+      timer = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1000);
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [countdown]);
+
+  useEffect(() => {
+    if (timeSessionStorage) {
+      const dif = new Date().getTime() - new Date(timeSessionStorage).getTime();
+
+      if (dif > compare) {
+        setButtonDisable(false);
+      }
+    }
+  }, [timeSessionStorage]);
+
+  const onClick = () => {
+    if (timeSessionStorage) {
+      const dif = new Date().getTime() - Number(timeSessionStorage);
+
+      if (dif > compare) {
+        setRefreshing(true);
+        callback();
+      } else {
+        setButtonDisable(true);
+      }
+    } else {
+      window.sessionStorage.setItem("refresh", new Date().getTime().toString());
+      setButtonDisable(true);
+      setRefreshing(true);
+      callback();
+    }
+    setRefreshing(false);
+  };
+
   return (
     <button
       className={`bg-slate-950 text-white py-2 px-4 rounded absolute top-4 right-4 ${
-        loading ? "disabled:opacity-50" : "hover:bg-slate-700"
+        buttonDisable ? "disabled:opacity-50" : "hover:bg-slate-700"
       }`}
-      onClick={callback}
-      disabled={loading}
+      onClick={() => onClick()}
+      disabled={buttonDisable}
     >
-      {loading ? "Refreshing..." : "Refresh"}
+      {buttonDisable &&
+        `Refresh available in ${Math.floor(countdown / 60000)}m ${Math.floor(
+          (countdown % 60000) / 1000
+        )}s`}
+      {!buttonDisable && (refreshing ? "Refreshing..." : "Refresh")}
     </button>
   );
 };
