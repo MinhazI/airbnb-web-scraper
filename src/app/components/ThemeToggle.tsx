98b6715f806622adca3d74a490e03770807f7396
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2025-02-20 13:12:13 +0530

    Added a skeleton for the theme toggle

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 import { FiMoon, FiSun } from "react-icons/fi";
 
 const ThemeToggle = () => {
@@ -14,14 +13,7 @@ const ThemeToggle = () => {
   if (!mounted)
     return (
       <div className="py-2">
-        <Image
-          src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-          height={36}
-          width={36}
-          alt="Loading dark or light theme"
-          priority={false}
-          title="Loading dark or light theme"
-        />
+        <div className="bg-gray-200 rounded-full dark:bg-gray-700 w-5 h-5 animate-pulse"></div>
       </div>
     );
   if (theme === "dark") {
